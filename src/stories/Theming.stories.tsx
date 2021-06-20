a8309aa42a5bf7f289a6a5a03d9665f40ebc4268
Deberson de Paula <deberson.paula@gmail.com>
2021-06-20 13:29:47 -0300

    added main stories

---
+++ src/stories/Theming.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { createTheme, StyleThemeProvider } from '../styles';
+import { StyleThemeProviderProps } from '../styles/StyleThemeProvider';
+import Button from '../components/Button';
+
+export default {
+  title: 'Styles/Theming/StyleThemeProvider',
+  component: StyleThemeProvider,
+} as Meta;
+
+const AppSample = () => (
+  <div>
+    <Button label="SAMPLE BUTTON" />
+  </div>
+);
+
+const Template: Story<StyleThemeProviderProps> = (args) => (
+  <StyleThemeProvider {...args}>
+    <AppSample />
+  </StyleThemeProvider>
+);
+
+export const BasicUsage = Template.bind({});
+BasicUsage.args = {
+  theme: createTheme(),
+};
